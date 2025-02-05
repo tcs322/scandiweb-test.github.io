eccce51c156bf765d63def36d64261d7c44c3895
Thyerre Sampaio <thyerre322@gmail.com>
2025-02-05 20:24:58 -0300

    adcionando validacao de rotas

--- app/routes/api.php
+++ app/routes/api.php
@@ -1,5 +1,8 @@
 <?php
 
-$route = ['/', 'HomeController@index'];
+$route[] = ['/', 'HomeController@index'];
+$route[] = ['/post', 'PostController@index'];
+$route[] = ['/post/{id}/show', 'PostController@show'];
+
 
 return $route;
--- core/Route.php
+++ core/Route.php
@@ -9,10 +9,49 @@ class Route
     public function __construct(array $routes)
     {
         $this->routes = $routes;
+        $this->run();
     }
 
-    public function getUrl()
+    private function getUrl()
     {
         return parse_url($_SERVER['REQUEST_URI'], PHP_URL_PATH);
+    }
+
+    private function splitUrl()
+    {
+        return explode('/', $this->getUrl());
+    }
+
+    private function run()
+    {
+        $urlArray = $this->splitUrl();
+
+        foreach ($this->routes as $route) {
+            $routeArray = explode('/', $route[0]);
+            echo '<br>';
+            // print_r($routeArray);
+
+            for($i = 0; $i < count($routeArray); $i++) {
+                if(strpos($routeArray[$i], "{") !== false && count($urlArray) == count($routeArray)) {
+                    $routeArray[$i] = $urlArray[$i];
+                }
+                $route[0] = implode('/', $routeArray);
+            }
+
+            if ($this->getUrl() == $route[0]) {
+                echo '<br>Rota válida';
+                echo '<br>';
+                echo $this->getUrl();
+                echo '<br>';
+                echo $route[0];
+                break;
+            } else {
+                echo '<br>Rota inválida';
+                echo '<br>';
+                echo $this->getUrl();
+                echo '<br>';
+                echo $route[0];
+            }
+        }
     }
 }
